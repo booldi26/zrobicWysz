24750148cc2c770c2e2069ab9a77e633128c54d3
booldi26 <144555972+booldi26@users.noreply.github.com>
2024-11-27 00:21:06 +0100

    Update stworz.js

--- stworz.js
+++ stworz.js
@@ -11,17 +11,24 @@ db.connect();
         console.log("utworzono")
     })
 
-    db.changeUser({ database: 'alert' }, (err) => {
+    db.changeUser({ database: 'alarm' }, (err) => {
         if(err) throw err;
         console.log("polączono")
     })
 
-    let sql = `INSERT INTO Dane(Nazwa, Stan_Ostrzegawczy, Stan_Alarmowy)
+    let sql1 = `CREATE TABLE Dane(Nazwa varchar(255), Stan_Ostrzegawczy decimal(65),Stan_Alarmowy decimal(65))`;
+
+    db.query(sql1, (err) => {
+        if(err) throw err;
+        console.log("dodano")
+    })
+
+    let sql2 = `INSERT INTO Dane(Nazwa, Stan_Ostrzegawczy, Stan_Alarmowy)
     VALUES 
     ('Sandomierz', 500, 530),
     ('Radom', 600, 680),
     ('Góra Kalwaria', 1000, 1050);`
-    db.query(sql, (err) => {
+    db.query(sql2, (err) => {
         if(err) throw err;
         console.log("dodano")
     })
